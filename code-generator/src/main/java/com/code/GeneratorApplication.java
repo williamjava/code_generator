245e35aefc3f9017745c35441d2df00ad24a6bc9
wuhoujian <wuhoujian@126.com>
2019-06-06 16:42:39 +0800

    代码完善

--- code-generator/src/main/java/com/code/GeneratorApplication.java
+++ code-generator/src/main/java/com/code/GeneratorApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 代码生成启动类
+ *
+ * 启动后访问：http://localhost:4000/code-generator
+ *
+ * 选择要生成的表，输入如下参数：
+ * 主目录：如com.tufire.user
+ * 模块名：如user
+ * 包名：如com.tufire.user
  * 
  * @author wuhoujian
  *
